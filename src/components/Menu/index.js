1690792d875729a7062e120189bc95f08be1afc8
Svetlana Slyusareva <svetlana.slyusareva@me.com>
2017-08-28 21:48:01 +0200

    added style-components

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import './style.css';
 
 class Menu extends Component {
   render() {
+    const MenuWrapper = styled.menu`
+      padding: 0;
+      font-size: 0;
+      box-sizing: border-box;
+      background: lightgrey;
+    `;
+
+    const MenuList = styled.li`
+      font-size: 16px;
+      display: inline-block;
+      width: 25%;
+      box-sizing: border-box;
+      text-align: center;
+    `;
+
+    const MenuLink = styled.a`
+      color: #000;
+      display: block;
+      padding: 20px;
+      text-decoration: none;
+    `;
+
     return (
       <nav>
-        <menu className="menu-list">
-          <li className="menu-list-item"><a href="#" className="menu-list-link">Fashion</a></li>
-          <li className="menu-list-item"><a href="#" className="menu-list-link">Beauty</a></li>
-          <li className="menu-list-item"><a href="#" className="menu-list-link">Fitness</a></li>
-          <li className="menu-list-item"><a href="#" className="menu-list-link">Lifestyle</a></li>
-        </menu>
+        <MenuWrapper>
+          <MenuList><MenuLink href="#">Fashion</MenuLink></MenuList>
+          <MenuList><MenuLink href="#">Beauty</MenuLink></MenuList>
+          <MenuList><MenuLink href="#">Fitness</MenuLink></MenuList>
+          <MenuList><MenuLink href="#">Lifestyle</MenuLink></MenuList>
+        </MenuWrapper>
       </nav>
     );
   }
