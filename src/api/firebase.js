03ad480316e422e5b38dae20711e76312a2054e1
Svetlana Slyusareva <svetlana.slyusareva@me.com>
2017-09-03 20:34:07 +0200

    added firebase

---
+++ src/api/firebase.js
@@ -0,0 +1,10 @@
+import Firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyBb6oGg59mz_YKEjgNUfWHqe2DoBccQvT0',
+  authDomain: 'blog-style-20507.firebaseapp.com',
+};
+
+const firebase = Firebase.initializeApp(config);
+
+export default firebase;
