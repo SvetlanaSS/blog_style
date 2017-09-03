400f371c102e5d74bbfb1d2c182675edad5ed3ee
Svetlana Slyusareva <svetlana.slyusareva@me.com>
2017-09-03 20:04:04 +0200

    login form

--- src/components/AuthorisationForm/LoginForm/index.js
+++ src/components/AuthorisationForm/LoginForm/index.js
@@ -1,6 +1,55 @@
 import React, { Component } from 'react';
 
 class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
+
+  handleOnChange = (e) => {
+    this.setState({ [e.target.name] : e.target.value });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <section>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="email"
+            className="form-control"
+            placeholder="Email"
+            onChange={this.handleOnChange}
+          />
+          <input
+            type="text"
+            name="password"
+            className="form-control"
+            placeholder="Password"
+            onChange={this.handleOnChange}
+          />
+          <input
+            type="submit"
+            className="btn"
+            value="Log in"
+          />
+        </form>
+      </section>
+    );
+  }
+}
+
+export default LoginForm;
+
+
 
   // const MiddleDiv = styled.div `
   //   height: 200px;
@@ -13,16 +62,3 @@ class LoginForm extends Component {
   //   padding-left: 5rem;
   //   padding-top: 2rem;
   // `;
-
-
-
-  render() {
-    return (
-      <section>
-        LoginForm
-      </section>
-    )
-  }
-}
-
-export default LoginForm;
