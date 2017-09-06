a99ea9594bb6d8fc4da8efd1b75661ac2e9f7e48
Svetlana Slyusareva <svetlana.slyusareva@me.com>
2017-09-06 21:23:04 +0200

    Main page mobile view

--- src/components/AuthorisationForm/index.js
+++ src/components/AuthorisationForm/index.js
@@ -6,20 +6,17 @@ import LoginForm from './LoginForm';
 class LoginWindow extends Component {
   render() {
     const WindowForm = styled.div`
-      width: 40rem;
-      height: 40rem;
+      width: 100%;
+      padding: 2rem;
       background: #FCFBFB;
-      margin: 0 auto;
+      margin: 10rem auto;
       display: flex;
       flex-direction: column;
       justify-content: center;
-      margin-top: 10rem;
-      margin-bottom: 10rem;
-    `;
 
-    const WindowWrapper = styled.div`
-      padding-left: 3rem;
-      padding-right: 3rem;
+      @media(min-width: 768px) {
+        width: 50%;
+      }
     `;
 
     const BottomDiv = styled.div `
@@ -30,11 +27,11 @@ class LoginWindow extends Component {
 
     return (
       <WindowForm>
-        <WindowWrapper>
-          <FacebookAuthorisation />
-          <LoginForm />
-          <BottomDiv>By creating an account, I accept BlogStyle's Terms of Service</BottomDiv>
-        </WindowWrapper>
+        <FacebookAuthorisation />
+        <LoginForm />
+        <BottomDiv>
+          By creating an account, I accept BlogStyle's Terms of Service
+        </BottomDiv>
       </WindowForm>
     );
   }
