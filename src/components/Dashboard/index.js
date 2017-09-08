7ba71fc4db486c0ca0ae1cc7c301575320d3fc01
Svetlana Slyusareva <svetlana.slyusareva@me.com>
2017-09-08 18:21:18 +0200

    wip dachboard

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,12 +1,52 @@
 import React, { Component } from 'react';
+import { Grid, Row, Col, Thumbnail, Button } from 'react-bootstrap';
 import Menu from '../Menu';
 
 class Dashboard extends Component {
   render() {
+    const thumbnailInstance = (
+      <Grid>
+        <Row>
+          <Col xs={6} md={4}>
+            <Thumbnail src="/assets/thumbnaildiv.png" alt="242x200">
+              <h3>Thumbnail label</h3>
+              <p>Description</p>
+              <p>
+                <Button bsStyle="primary">Button</Button>&nbsp;
+                <Button bsStyle="default">Button</Button>
+              </p>
+            </Thumbnail>
+          </Col>
+          <Col xs={6} md={4}>
+            <Thumbnail src="/assets/thumbnaildiv.png" alt="242x200">
+              <h3>Thumbnail label</h3>
+              <p>Description</p>
+              <p>
+                <Button bsStyle="primary">Button</Button>&nbsp;
+                <Button bsStyle="default">Button</Button>
+              </p>
+            </Thumbnail>
+          </Col>
+          <Col xs={6} md={4}>
+            <Thumbnail src="/assets/thumbnaildiv.png" alt="242x200">
+              <h3>Thumbnail label</h3>
+              <p>Description</p>
+              <p>
+                <Button bsStyle="primary">Button</Button>&nbsp;
+                <Button bsStyle="default">Button</Button>
+              </p>
+            </Thumbnail>
+          </Col>
+        </Row>
+      </Grid>
+    );
     return (
       <section>
         <Menu />
-        dashboard, i am logged in
+        { thumbnailInstance }
+        { thumbnailInstance }
+        { thumbnailInstance }
+        { thumbnailInstance }
       </section>
     );
   }
