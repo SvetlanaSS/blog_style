cb6c99d092901bf45dd698b2937432e859a46459
Svetlana Slyusareva <svetlana.slyusareva@me.com>
2017-09-11 16:53:39 +0200

    max length for post and more content for data.json

--- src/components/BlogPost/index.js
+++ src/components/BlogPost/index.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react';
-import { Row, Col, Thumbnail, Button } from 'react-bootstrap';
+import { Grid, Row, Col, Thumbnail, Button } from 'react-bootstrap';
 
 class BlogPost extends Component {
   render() {
     const { post } = this.props;
     const thumbnailInstance = (
-      <Row>
-        <Col xs={6} md={4}>
-          <Thumbnail src={post.image_url} alt="foto" style={{width: '242px', heigth: '200px'}}>
-            <h4>{post.title}</h4>
-            <p style={{color: 'grey', fontSize: '1rem'}}>Published: {post.date}</p>
-            <p>{post.description}</p>
-            <p style={{color: 'grey'}}>{post.hashtag}</p>
-            <p>
-              <Button bsStyle="default">Button</Button>&nbsp;
-              <Button bsStyle="default">Button</Button>
-            </p>
-          </Thumbnail>
-        </Col>
-      </Row>
+      <Grid>
+        <Row>
+          <Col xs={6} md={3}>
+            <Thumbnail src={post.image_url} alt="foto" style={{width: '242px', heigth: '200px'}}>
+              <h4>{post.title}</h4>
+              <p style={{color: 'grey', fontSize: '1rem'}}>Published: {post.date}</p>
+              <p>{post.description.slice(0, 130) + '...'}</p>
+              <p style={{color: 'grey'}}>{post.hashtag}</p>
+              <p>
+                <Button bsStyle="default">Button</Button>&nbsp;
+                <Button bsStyle="default">Button</Button>
+              </p>
+            </Thumbnail>
+          </Col>
+        </Row>
+      </Grid>
     );
     return (
       <div>
