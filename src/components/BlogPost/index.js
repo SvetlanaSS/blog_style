804ad2aff884e3f58b3289815f98e03c8159f1b7
Svetlana Slyusareva <svetlana.slyusareva@me.com>
2017-09-12 13:29:48 +0200

    Render blogpost cards

--- src/components/BlogPost/index.js
+++ src/components/BlogPost/index.js
@@ -2,29 +2,25 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { showModal } from '../../actions/modal';
-import { Grid, Row, Col, Thumbnail, Button } from 'react-bootstrap';
+import { Col, Thumbnail, Button } from 'react-bootstrap';
 
 class BlogPost extends Component {
 
   render() {
     const { post, showModal } = this.props;
     const thumbnailInstance = (
-      <Grid>
-        <Row>
-          <Col xs={6} md={3}>
-            <Thumbnail src={post.image_url} alt="foto" style={{width: '242px', heigth: '200px'}}>
-              <h4><a style={{color: 'black', cursor: 'pointer'}} onClick={() => showModal(post)}>{post.title}</a></h4>
-              <p style={{color: 'grey', fontSize: '1rem'}}>Published: {post.date}</p>
-              <p>{post.description.slice(0, 130) + '...'}</p>
-              <p style={{color: 'grey'}}>{post.hashtag}</p>
-              <p>
-                <Button bsStyle="default">Button</Button>&nbsp;
-                <Button bsStyle="default">Button</Button>
-              </p>
-            </Thumbnail>
-          </Col>
-        </Row>
-      </Grid>
+      <Col xs={12} sm={6} md={3} >
+        <Thumbnail src={post.image_url} alt="foto" style={{width: '242px', heigth: '200px', minHeight: '450px', maxHeight: '450px'}}>
+          <h4><a style={{color: 'black', cursor: 'pointer'}} onClick={() => showModal(post)}>{post.title}</a></h4>
+          <p style={{color: 'grey', fontSize: '1rem'}}>Published: {post.date}</p>
+          <p>{post.description.slice(0, 130) + '...'}</p>
+          <p style={{color: 'grey'}}>{post.hashtag}</p>
+          <p>
+            <Button bsStyle="default">Button</Button>&nbsp;
+            <Button bsStyle="default">Button</Button>
+          </p>
+        </Thumbnail>
+      </Col>
     );
     return (
       <div>
--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,39 +1,55 @@
 import React, { Component } from 'react';
 import firebase from '../../api/firebase';
 import BlogPost from '../BlogPost';
+import { Grid, Row } from 'react-bootstrap';
 
 class Page extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      fashion: {},
-      beauty: {},
-      fitness: {}
+      fashion: [],
+      beauty: [],
+      fitness: []
     };
   }
 
   componentDidMount() {
     return firebase.database().ref('/fitness/').once('value').then(data => {
-      this.setState({ fitness: data.val() });
+      const response = data.val();
+      this.setState({
+        fitness: response.fitness,
+        beauty: response.beauty,
+        fashion: response.fashion,
+      });
     });
   }
 
   renderPosts = () => {
     const { fitness } = this.state;
-    if (Object.keys(fitness).length) {
-      return Object.keys(fitness).map((key)  => {
-        return <BlogPost key={key} post={fitness[key]} />;
+    if (fitness.length) {
+      const obj = {};
+      let i, j, chunk = 4;
+      for (i = 0, j = fitness.length; i < j; i += chunk) {
+        obj[i] = fitness.slice(i, i + chunk);
+      }
+
+      return Object.keys(obj).map(key => {
+        return (
+          <Row key={key} className="show-grid">
+            {obj[key].map(post => {
+              return <BlogPost key={post.id} post={post} />;
+            })}
+          </Row>
+        );
       });
     }
   }
 
   render() {
     return (
-      <section className="columns">
-        <div className="column">
-          {this.renderPosts()}
-        </div>
-      </section>
+      <Grid>
+        {this.renderPosts()}
+      </Grid>
     );
   }
 }
