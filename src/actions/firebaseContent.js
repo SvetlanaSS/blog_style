721c5888b19bd0c5a9d4d6aae44f6900bf1eb926
Svetlana Slyusareva <svetlana.slyusareva@me.com>
2017-09-12 14:21:43 +0200

    Test if data is already loaded

--- src/actions/firebaseContent.js
+++ src/actions/firebaseContent.js
@@ -5,14 +5,17 @@ import {
 } from './types';
 
 export function fetchDataFirebase() {
-  return dispatch => {
-    firebase.database().ref('/fitness/').once('value')
-      .then(data => {
-        const response = data.val();
-        dispatch({ type: FETCH_DATA_FROM_FIREBASE_SUCCESS, response });
-      })
-      .catch(error => {
-        dispatch({ type: FETCH_DATA_FROM_FIREBASE_ERROR, error });
-      });
+  return (dispatch, getState) => {
+    const { firebaseContent } = getState();
+    if (!firebaseContent.fashion.length) {
+      firebase.database().ref('/').once('value')
+        .then(data => {
+          const response = data.val();
+          dispatch({ type: FETCH_DATA_FROM_FIREBASE_SUCCESS, response });
+        })
+        .catch(error => {
+          dispatch({ type: FETCH_DATA_FROM_FIREBASE_ERROR, error });
+        });
+    }
   };
 }
