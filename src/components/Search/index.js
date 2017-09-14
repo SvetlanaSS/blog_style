fbb0c4511056166d60cd328ee44e4dcea8badf7d
Svetlana Slyusareva <svetlana.slyusareva@me.com>
2017-09-14 19:59:39 +0200

    Controll open/close from redux

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,39 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { showModalSearch } from '../../actions/modalSearch';
+import FaClose from 'react-icons/lib/fa/close';
+import { hideModalSearch } from '../../actions/modalSearch';
 import { Panel, Collapse } from 'react-bootstrap';
-import FaSearch from 'react-icons/lib/fa/search';
 import SearchByToday from './searchByToday';
 import SearchByHashtag from './searchByHashtag';
 import SearchByMestLikes from './searchByMestLikes';
 
-
 class Search extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showSearch: false
-    };
-  }
-
-  toggleSearch = () => {
-    const { showSearch } = this.state;
-    if (showSearch) {
-      this.setState({ showSearch: false });
-    } else {
-      this.setState({ showSearch: true });
-    }
-  }
-
   render() {
-    const { showSearch } = this.state;
+    const { hideModalSearch, showSearch } = this.props;
     const wellStyles = {maxWidth: 400, margin: '0 auto 1rem'};
     return (
-      <div style={wellStyles}>
-        <FaSearch size={25} onClick={()=> this.toggleSearch()} />
+      <div onClick={() => hideModalSearch()} style={wellStyles}>
         <Collapse in={showSearch}>
           <div>
+            <span><FaClose/></span>
             <Panel>
               <div style={{display: 'inline-block'}}>
                 <SearchByToday />
@@ -49,8 +32,14 @@ class Search extends Component {
   }
 }
 
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ showModalSearch }, dispatch);
+function mapStateToProps(state) {
+  return {
+    showSearch: state.modalSearch.showModalSearch
+  };
 }
 
-export default connect(null, mapDispatchToProps)(Search);
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ hideModalSearch }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Search);
