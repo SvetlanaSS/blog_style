ed1bac1d860b3912d8e272ce8f7c025d319002ee
Svetlana Slyusareva <svetlana.slyusareva@me.com>
2017-09-16 15:34:44 +0200

    created likes to post

--- src/components/BlogPost/index.js
+++ src/components/BlogPost/index.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { showModalPost } from '../../actions/modalPost';
-import { Col, Thumbnail, Button } from 'react-bootstrap';
+import { Col, Thumbnail } from 'react-bootstrap';
+import FaHeartO from 'react-icons/lib/fa/heart-o';
 
 class BlogPost extends Component {
 
@@ -10,15 +11,14 @@ class BlogPost extends Component {
     const { post, showModalPost } = this.props;
     const thumbnailInstance = (
       <Col xs={12} sm={6} md={3} >
-        <Thumbnail src={post.image_url} alt="foto" style={{width: '242px', heigth: '200px', minHeight: '470px', maxHeight: '470px'}}>
+        <Thumbnail src={post.image_url} alt="foto" style={{width: '242px', heigth: '200px', minHeight: '500px', maxHeight: '500px'}}>
           <h4><a style={{color: 'black', cursor: 'pointer'}} onClick={() => showModalPost(post)}>{post.title}</a></h4>
           <p style={{color: 'grey', fontSize: '1rem'}}>Published: {post.date}</p>
           <p>{post.description.slice(0, 130) + '...'}</p>
           <p style={{color: 'grey'}}>{post.hashtag}</p>
-          <p>
-            <Button bsStyle="default">Button</Button>&nbsp;
-            <Button bsStyle="default">Button</Button>
-          </p>
+          <p style={{color: 'grey'}}>{post.likes} Likes</p>
+          <hr/>
+          <FaHeartO style={{color: 'grey'}} />
         </Thumbnail>
       </Col>
     );
