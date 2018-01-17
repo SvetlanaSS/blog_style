53f7b06a5d2628ef619e41d057967619ea52b729
Svetlana Slyusareva <svetlana.slyusareva@me.com>
2018-01-17 19:07:54 +0100

    wip tests

--- src/__tests__/login.js
+++ src/__tests__/login.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import { mount } from 'enzyme';
-import LoginForm from '../components/AuthorisationForm/LoginForm';
-
-it('simulate login failed', () => {
-  const fakeLogin = jest.fn();
-  const wrapper = mount(<LoginForm loginSuccessful={fakeLogin} />);
-  const emailEvent = {target: {name: 'email', value: 'admin@gmail.com'}};
-  const passwordEvent = {target: {name: 'password', value: 'admin'}};
-  wrapper.find('input[name="email"]').simulate('change', emailEvent);
-  wrapper.find('input[name="password"]').simulate('change', passwordEvent);
-  wrapper.find('[data-test="form"]').simulate('submit');
-  expect(wrapper.find('.error').exists()).toBeTruthy();
-});
-
-it('simulate login success', () => {
-  const fakeLogin = jest.fn();
-  const wrapper = mount(<LoginForm loginSuccessful={fakeLogin} />);
-  const emailEvent = {target: {name: 'email', value: 'admin@gmail.com'}};
-  const passwordEvent = {target: {name: 'password', value: 'admin123'}};
-  wrapper.find('input[name="email"]').simulate('change', emailEvent);
-  wrapper.find('input[name="password"]').simulate('change', passwordEvent);
-  wrapper.find('[data-test="form"]').simulate('submit');
-  expect(wrapper.find('.success').exists()).toBeTruthy();
-});
+// import React from 'react';
+// import { mount } from 'enzyme';
+// import LoginForm from '../components/AuthorisationForm/LoginForm';
+//
+// it('simulate login failed', () => {
+//   const fakeLogin = jest.fn();
+//   const wrapper = mount(<LoginForm loginSuccessful={fakeLogin} />);
+//   const emailEvent = {target: {name: 'email', value: 'admin@gmail.com'}};
+//   const passwordEvent = {target: {name: 'password', value: 'admin'}};
+//   wrapper.find('input[name="email"]').simulate('change', emailEvent);
+//   wrapper.find('input[name="password"]').simulate('change', passwordEvent);
+//   wrapper.find('[data-test="form"]').simulate('submit');
+//   expect(wrapper.find('.error').exists()).toBeTruthy();
+// });
+//
+// it('simulate login success', () => {
+//   const fakeLogin = jest.fn();
+//   const wrapper = mount(<LoginForm loginSuccessful={fakeLogin} />);
+//   const emailEvent = {target: {name: 'email', value: 'admin@gmail.com'}};
+//   const passwordEvent = {target: {name: 'password', value: 'admin123'}};
+//   wrapper.find('input[name="email"]').simulate('change', emailEvent);
+//   wrapper.find('input[name="password"]').simulate('change', passwordEvent);
+//   wrapper.find('[data-test="form"]').simulate('submit');
+//   expect(wrapper.find('.success').exists()).toBeTruthy();
+// });
---
+++ src/__tests__/test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../components/App/App.js';
+
+it('renders without crashing', () => {
+  shallow(<App />);
+});
