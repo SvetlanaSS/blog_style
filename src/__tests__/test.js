a46ee9b6560e75cbab8c75da589eedf7afc85adb
Svetlana Slyusareva <svetlana.slyusareva@me.com>
2018-01-20 17:43:09 +0100

    Shallow test App component

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../components/App/App.js';
+import reduxStore from '../store';
+const store = reduxStore();
 
 it('renders without crashing', () => {
-  shallow(<App />);
+  shallow(<App store={store} />);
 });
