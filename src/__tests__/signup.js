77314834fc77f87e4690f3b8e8c3fbe1886b0b25
Svetlana Slyusareva <svetlana.slyusareva@me.com>
2018-01-21 12:45:19 +0100

    test signup failed with an invalid email

--- src/__tests__/signup.js
+++ src/__tests__/signup.js
@@ -13,20 +13,20 @@ it('simulate signup failed with an empty name', () => {
   expect(wrapper.find('input[type="submit"]').props().disabled).toBe(true);
 });
 
-it('simulate signup failed with an unvalid short password', () => {
+it('simulate signup failed with an invalid emai', () => {
+  const wrapper = mount(<SignUpForm signUpUser={jest.fn()} />);
+  wrapper.find('input[name="name"]').simulate('change', nameEvent);
+  wrapper.find('input[name="email"]').simulate('change', { target: { name: 'email', value: 'admin.com' }});
+  wrapper.find('input[name="password"]').simulate('change', passwordEvent);
+  expect(wrapper.find('.hasDanger').exists()).toBeTruthy();
+  expect(wrapper.find('input[name="email"]').closest('div').text()).toEqual('Email is not valid!');
+});
+
+it('simulate signup failed with an invalid short password', () => {
   const wrapper = mount(<SignUpForm signUpUser={jest.fn()} />);
   wrapper.find('input[name="name"]').simulate('change', nameEvent);
   wrapper.find('input[name="email"]').simulate('change', emailEvent);
   wrapper.find('input[name="password"]').simulate('change', { target: { name: 'password', value: '123' }});
   expect(wrapper.find('.hasDanger').exists()).toBeTruthy();
   expect(wrapper.find('input[name="password"]').closest('div').text()).toEqual('Password is too short!');
-  //expect(wrapper.find('div').text());
-});
-
-it('', () => {
-
-});
-
-it('', () => {
-
 });
