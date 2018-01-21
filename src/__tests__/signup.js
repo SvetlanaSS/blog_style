cdef239580bfe86d3fe07aa1333125174c9c628b
Svetlana Slyusareva <svetlana.slyusareva@me.com>
2018-01-21 12:40:33 +0100

    test signup failed with an unvalid short password

--- src/__tests__/signup.js
+++ src/__tests__/signup.js
@@ -2,13 +2,31 @@ import React from 'react';
 import { mount } from 'enzyme';
 import { SignUpForm } from '../components/AuthorisationForm/SignUpForm';
 
-// const nameEvent = { target: { name: 'name', value: 'John Doe' }};
+const nameEvent = { target: { name: 'name', value: 'John Doe' }};
 const emailEvent = { target: { name: 'email', value: 'admin@gmail.com' }};
 const passwordEvent = { target: { name: 'password', value: 'password' }};
 
-it('simulate signup failed without a name', () => {
+it('simulate signup failed with an empty name', () => {
   const wrapper = mount(<SignUpForm signUpUser={jest.fn()} />);
   wrapper.find('input[name="email"]').simulate('change', emailEvent);
   wrapper.find('input[name="password"]').simulate('change', passwordEvent);
   expect(wrapper.find('input[type="submit"]').props().disabled).toBe(true);
+});
+
+it('simulate signup failed with an unvalid short password', () => {
+  const wrapper = mount(<SignUpForm signUpUser={jest.fn()} />);
+  wrapper.find('input[name="name"]').simulate('change', nameEvent);
+  wrapper.find('input[name="email"]').simulate('change', emailEvent);
+  wrapper.find('input[name="password"]').simulate('change', { target: { name: 'password', value: '123' }});
+  expect(wrapper.find('.hasDanger').exists()).toBeTruthy();
+  expect(wrapper.find('input[name="password"]').closest('div').text()).toEqual('Password is too short!');
+  //expect(wrapper.find('div').text());
+});
+
+it('', () => {
+
+});
+
+it('', () => {
+
 });
