001395e491e2d6927775cdcf06b3cb8c1106389e
Svetlana Slyusareva <svetlana.slyusareva@me.com>
2018-01-22 20:31:45 +0100

    test footer

--- src/__tests__/Footer/index.test.js
+++ src/__tests__/Footer/index.test.js
@@ -1,10 +1,15 @@
-// import React from 'react';
-// import Footer from '../../components/Footer';
-// import { shallow } from 'enzyme';
-//
-// it('', () => {
-//   const wrapper = shallow(<Footer />);
-//   const faceebook = 'https://www.facebook.com/';
-//   wrapper.find('a').simulate('click');
-//   // expect(wrapper.)
-// });
+import React from 'react';
+import Footer from '../../components/Footer';
+import { mount } from 'enzyme';
+
+fit('a tags contain social media links', () => {
+  const result = [
+    'https://www.facebook.com/',
+    'https://www.instagram.com',
+    'https://twitter.com',
+    'https://www.pinterest.com/'
+  ];
+  const wrapper = mount(<Footer />);
+  const socialUrls = wrapper.find('a').map(node => node.props().href);
+  expect(socialUrls).toEqual(result);
+});
