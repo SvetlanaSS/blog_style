5b154335991261865c05c85e4d8f41fcf0339c1e
Svetlana Slyusareva <svetlana.slyusareva@me.com>
2018-01-24 21:19:24 +0100

    test header contains logo

--- src/__tests__/components/Header/index.test.js
+++ src/__tests__/components/Header/index.test.js
@@ -5,3 +5,8 @@ import { shallow } from 'enzyme';
 it('renders without a error', () => {
   shallow(<Header />);
 });
+
+it('header contains logo', () => {
+  const wrapper = shallow(<Header />);
+  expect(wrapper.find('img').html()).toContain('<img src="logo.png" alt="logo"/>');
+});
