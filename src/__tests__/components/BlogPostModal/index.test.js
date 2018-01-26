6b1a90d7a2a73f5062e2b68680b1fea3fa7a87bf
Svetlana Slyusareva <svetlana.slyusareva@me.com>
2018-01-26 21:46:45 +0100

    test render comments in blogPostModal

--- src/__tests__/components/BlogPostModal/index.test.js
+++ src/__tests__/components/BlogPostModal/index.test.js
@@ -41,3 +41,43 @@ it('should render multiple likes in the  blogpost modal', () => {
   );
   expect(wrapper.instance().renderLikes().props.children).toEqual(expect.arrayContaining([likes.length, ' Likes']));
 });
+
+it('should render one comment in the blogpost modal', () => {
+  const comment = ['awesome comment'];
+  const post = {
+    title: 'post title',
+    image_url: 'url',
+    hashtag: 'hashtag',
+    description: 'description',
+    comments: comment,
+    likes: ['like']
+  };
+
+  const wrapper = mount(
+    <BlogPostModal
+      post={post}
+      showModalPost={true}
+    />
+  );
+  expect(wrapper.instance().renderComments().props.children).toEqual(expect.arrayContaining([comment.length, ' Comment']));
+});
+
+it('should render multiple comments in the  blogpost modal', () => {
+  const comments = ['comment', 'comment', 'comment', 'comment'];
+  const post = {
+    title: 'post title',
+    image_url: 'url',
+    hashtag: 'hashtag',
+    description: 'description',
+    comments,
+    likes: ['like']
+  };
+
+  const wrapper = mount(
+    <BlogPostModal
+      post={post}
+      showModalPost={true}
+    />
+  );
+  expect(wrapper.instance().renderComments().props.children).toEqual(expect.arrayContaining([comments.length, ' Comments']));
+});
