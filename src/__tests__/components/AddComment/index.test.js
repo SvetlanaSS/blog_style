bfe578d0644f6b676f825468d15eda14cf7c6228
Svetlana Slyusareva <svetlana.slyusareva@me.com>
2018-01-26 20:50:48 +0100

    test addComment

--- src/__tests__/components/AddComment/index.test.js
+++ src/__tests__/components/AddComment/index.test.js
@@ -2,18 +2,23 @@ import React from 'react';
 import { mount } from 'enzyme';
 import { AddComment } from '../../../components/AddComment';
 
-const comment = 'Nice post';
+beforeEach(() => localStorage.clear());
+afterEach(() => localStorage.clear());
 
 fit('simulate add comment', () => {
-  const wrapper = mount(<AddComment />);
-  const commentEvent = {target: {name: 'comment', value: comment}};
-
-
-  //console.log(wrapper.find('textarea[name="comment"]').simulate('change', commentEvent));
-  //wrapper.find('textarea[name="comment"]').simulate('change', commentEvent);
-  //console.log(wrapper.find('textarea[name="comment"]').simulate('change', commentEvent));
-  //console.log(wrapper.find('data-test="form"')).simulate('submit');
-  //console.log(wrapper.find('button').at(0).props());
-  //wrapper.find('button').at(0).simulate('submit');
-  //expect(wrapper.state('comment')).toBe(comment);
+  const user = JSON.stringify({ email: 'test@test.com', id: 1 });
+  localStorage.setItem('firebase:authUser:', user);
+  const wrapper = mount(
+    <AddComment
+      hideModalAddComment={jest.fn()}
+      forceFetchDataFirebase={jest.fn()}
+      showModalAddComment={true}
+      post={ { id: 1 } }
+    />
+  );
+  const comment = 'Nice post';
+  const commentEvent = { target: { name: 'comment', value: comment }};
+  wrapper.find('textarea[name="comment"]').simulate('change', commentEvent);
+  wrapper.find('[data-test="form"]').simulate('submit');
+  expect(wrapper.state('comment')).toBe(comment);
 });
