3b342d6fa4c3c3e5158f3894db20cb5ad4b7d68b
Svetlana Slyusareva <svetlana.slyusareva@me.com>
2018-01-27 19:42:13 +0100

    test blogpost

--- src/__tests__/components/BlogPost/index.test.js
+++ src/__tests__/components/BlogPost/index.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { BlogPost } from '../../../components/BlogPost';
 
 it('renders emails of persons who liked the post', () => {
@@ -31,4 +31,42 @@ it('renders emails of persons who liked the post', () => {
   const childComp = wrapper.instance().renderPopoverLikes(likes).props.children;
   const arrayOfEmails = childComp.map(node => node.props.children);
   expect(arrayOfEmails).toEqual([firstEmail, secondEmail]);
+});
+
+
+it('renders emails and comments props', () => {
+  const user = JSON.stringify({ email: 'test@test.com' });
+  localStorage.setItem('firebase:authUser:', user);
+
+  const firstEmail = 'firstEmail@test.com';
+  const secondEmail = 'secondEmail@test.com';
+  const firstComment = 'Nice post!';
+  const secondComment = 'Jag vill ha det';
+  const comments = {
+    comment1: { email: firstEmail, comment: firstComment },
+    comment2: { email: secondEmail, comment: secondComment }
+  };
+  const likes = [{ email: firstEmail }, { email: secondEmail }];
+
+  const post = {
+    title: 'post title',
+    image_url: 'url',
+    hashtag: 'hashtag',
+    description: 'description',
+    comments,
+    likes
+  };
+
+  const wrapper = mount(
+    <BlogPost
+      post={post}
+      showModalPost={true}
+      forceFetchDataFirebase={jest.fn()}
+      showModalAddComment={true}
+      location={{ pathname: 'fashion' }}
+    />
+  );
+
+  expect(wrapper.props().post.comments).toEqual(comments);
+  expect(wrapper.props().post.likes).toEqual(likes);
 });
