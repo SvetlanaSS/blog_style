5a8647ddf830ac6e93abd1ae51bacad7a2490ffb
Svetlana Slyusareva <svetlana.slyusareva@me.com>
2018-01-29 12:43:19 +0100

    test connected app

--- src/__tests__/components/App/index.test.js
+++ src/__tests__/components/App/index.test.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { App } from '../../../components/App/App.js';
+import ConnectedApp, { App } from '../../../components/App/App.js';
+import configureStore from 'redux-mock-store';
 
 it('renders without crashing', () => {
   shallow(<App />);
+});
+
+describe('Shallow + passing the {store} directly', () =>{
+  const initialState = { auth: { authenticated: true } };
+  const mockStore = configureStore();
+  let store, wrapper;
+
+  beforeEach(()=>{
+    store = mockStore(initialState);
+    wrapper = shallow(<ConnectedApp store={store} /> );
+  });
+
+  it('render the connected component', () => {
+    expect(wrapper.length).toEqual(1);
+  });
 });
