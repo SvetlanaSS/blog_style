8af56c22ada917aee793131ab0eb079e5f2e315f
Svetlana Slyusareva <svetlana.slyusareva@me.com>
2018-01-29 14:09:17 +0100

    test connected facebook authorisation

--- src/__tests__/components/FacebookAuthorisation/index.test.js
+++ src/__tests__/components/FacebookAuthorisation/index.test.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { FacebookAuthorisation } from '../../../components/AuthorisationForm/FacebookAuthorisation';
+import ConnectedFacebookAuthorisation, { FacebookAuthorisation } from '../../../components/AuthorisationForm/FacebookAuthorisation';
+import configureStore from 'redux-mock-store';
 
 it('renders without an error', () => {
   shallow(<FacebookAuthorisation />);
+});
+
+describe('Shallow + passing the {store} directly', () =>{
+  const initialState = { auth: { authenticated: true } };
+  const mockStore = configureStore();
+  let store, wrapper;
+
+  beforeEach(()=>{
+    store = mockStore(initialState);
+    wrapper = shallow(<ConnectedFacebookAuthorisation store={store} /> );
+  });
+
+  it('render the connected component', () => {
+    expect(wrapper.length).toEqual(1);
+  });
 });
