3a7aa85e2c1ce6bab76ae904a3ebc207e64c2f0a
Svetlana Slyusareva <svetlana.slyusareva@me.com>
2018-01-29 13:58:50 +0100

    test connected header

--- src/__tests__/components/Header/index.test.js
+++ src/__tests__/components/Header/index.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Header } from '../../../components/Header';
+import ConnectedHeader, { Header } from '../../../components/Header';
 import { shallow } from 'enzyme';
+import configureStore from 'redux-mock-store';
 
 it('renders without a error', () => {
   shallow(<Header />);
@@ -9,4 +10,19 @@ it('renders without a error', () => {
 it('header contains logo', () => {
   const wrapper = shallow(<Header />);
   expect(wrapper.find('img').html()).toContain('<img src="logo.png" alt="logo"/>');
+});
+
+describe('Shallow + passing the {store} directly', () =>{
+  const initialState = { auth: { authenticated: true } };
+  const mockStore = configureStore();
+  let store, wrapper;
+
+  beforeEach(()=>{
+    store = mockStore(initialState);
+    wrapper = shallow(<ConnectedHeader store={store} /> );
+  });
+
+  it('render the connected component', () => {
+    expect(wrapper.length).toEqual(1);
+  });
 });
