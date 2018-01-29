ea4af95ae31c695417b8702c30d890fee99f93be
Svetlana Slyusareva <svetlana.slyusareva@me.com>
2018-01-29 14:01:59 +0100

    test connected loader

--- src/__tests__/components/Loader/index.test.js
+++ src/__tests__/components/Loader/index.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Loader } from '../../../components/Loader';
+import ConnectedLoader, { Loader } from '../../../components/Loader';
+import configureStore from 'redux-mock-store';
 
 it('does not show loader on false props', () => {
   const loader = { showLoader: false };
@@ -12,4 +13,19 @@ it('shows loader on true props', () => {
   const loader = { showLoader: true };
   const wrapper = shallow(<Loader loader={loader} />);
   expect(wrapper.hasClass('loading')).toBe(true);
+});
+
+describe('Shallow + passing the {store} directly', () =>{
+  const initialState = { loader: true };
+  const mockStore = configureStore();
+  let store, wrapper;
+
+  beforeEach(()=>{
+    store = mockStore(initialState);
+    wrapper = shallow(<ConnectedLoader store={store} /> );
+  });
+
+  it('render the connected component', () => {
+    expect(wrapper.length).toEqual(1);
+  });
 });
